ca05cda7458d2be276bb0ae812744ad9e4f49fa9
Erik Schnetter <schnetter@gmail.com>
2021-07-08 17:42:54 -0400

    Only test formats that are supported

--- test/Format.jl
+++ test/Format.jl
@@ -14,13 +14,17 @@ function test_Format()
         @test hash(FORMAT_HDF5) isa UInt
         @test hash(FORMAT_HDF5) ≠ hash(convert(UInt, FORMAT_HDF5))
 
+        exts = get_file_extensions()
         for format in [FORMAT_HDF5, FORMAT_ADIOS1, FORMAT_ADIOS2, FORMAT_ADIOS2_SST, FORMAT_ADIOS2_SSC, FORMAT_JSON]
             suf = suffix(format)
             @test suf isa AbstractString
             @test !isempty(suf)
-            # Not all formats can be uniquely recognized by suffix
-            want_format = format == FORMAT_ADIOS1 ? FORMAT_ADIOS2 : format
-            @test determine_format("hello.$suf") == want_format
+            if suf ∈ exts
+                # Only test formats that are supported by this configuration
+                # Not all formats can be uniquely recognized by suffix
+                want_format = format == FORMAT_ADIOS1 ? FORMAT_ADIOS2 : format
+                @test determine_format("hello.$suf") == want_format
+            end
         end
     end
 end
